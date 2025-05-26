bece9de21357bff70e1261529323e1fbb8c1b60f
Adam Skorupski <adamskorupski@hotmail.com>
2025-05-26 22:59:51 +0200

    add sample data

--- CW-9-s29441/Data/AppDbContext.cs
+++ CW-9-s29441/Data/AppDbContext.cs
@@ -15,4 +15,52 @@ public class AppDbContext : DbContext
     {
         
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        var doctors = new List<Doctor>
+        {
+            new (){IdDoctor = 1, FirstName = "Adam", LastName = "Skorupski", Email = "s29441@pjwstk.edu.pl"},
+            new (){IdDoctor = 2, FirstName = "Tomasz", LastName = "Tomaszowski", Email = "tomasz@tomaszowski.edu.pl"},
+            new (){IdDoctor = 3, FirstName = "Anna", LastName = "Annowska", Email = "anna@annowska.pl"},
+        };
+
+        var patients = new List<Patient>
+        {
+            new (){IdPatient = 1, FirstName = "Magdalena", LastName = "Lena", Birthdate = new DateTime(2000,1,1)},
+            new (){IdPatient = 2, FirstName = "Katarzyna", LastName = "Kata", Birthdate = new DateTime(2000,2,2)},
+            new (){IdPatient = 3, FirstName = "Aleksandra", LastName = "Mandra", Birthdate = new DateTime(2000,3,3)}
+        };
+
+        var medicaments = new List<Medicament>
+        {
+            new (){IdMedicament = 1, Name = "APAP", Description = "Paracetamol", Type = "przeciwbólowy"},
+            new (){IdMedicament = 2, Name = "Ibuprom", Description = "Ibuprofen", Type = "przeciwbólowy"},
+            new (){IdMedicament = 3, Name = "Rutinoscorbin", Description = "Witamina C", Type = "odpornościowy"},
+        };
+
+        var prescriptions = new List<Prescription>
+        {
+            new (){IdPrescription = 1, Date = DateTime.Today, DueDate = DateTime.Today.AddDays(14), IdPatient = 1, IdDoctor = 1},
+            new (){IdPrescription = 2, Date = DateTime.Today.AddDays(-1), DueDate = DateTime.Today.AddDays(7), IdPatient = 1, IdDoctor = 2},
+            new (){IdPrescription = 3, Date = DateTime.Today.AddDays(-2), DueDate = DateTime.Today.AddDays(7), IdPatient = 2, IdDoctor = 2},
+            new (){IdPrescription = 4, Date = DateTime.Today.AddDays(-3), DueDate = DateTime.Today.AddDays(7), IdPatient = 3, IdDoctor = 3},
+        };
+
+        var prescriptionMedicaments = new List<PrescriptionMedicament>
+        {
+            new (){IdMedicament = 1, IdPrescription = 1, Dose = 5, Details = "codziennie wieczorem"},
+            new (){IdMedicament = 2, IdPrescription = 1, Details = "raz na tydzień"},
+            new (){IdMedicament = 2, IdPrescription = 2, Dose = 1, Details = "co 2 dni"},
+            new (){IdMedicament = 3, IdPrescription = 3, Dose = 2, Details = "codziennie rano"},
+        };
+        
+        modelBuilder.Entity<Doctor>().HasData(doctors);
+        modelBuilder.Entity<Patient>().HasData(patients);
+        modelBuilder.Entity<Medicament>().HasData(medicaments);
+        modelBuilder.Entity<Prescription>().HasData(prescriptions);
+        modelBuilder.Entity<PrescriptionMedicament>().HasData(prescriptionMedicaments);
+    }
 }
---
+++ CW-9-s29441/Migrations/20250526205204_dodanie danych testowych.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CW_9_s29441.Migrations
+{
+    /// <inheritdoc />
+    public partial class dodaniedanychtestowych : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Doctor",
+                columns: new[] { "IdDoctor", "Email", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, "s29441@pjwstk.edu.pl", "Adam", "Skorupski" },
+                    { 2, "tomasz@tomaszowski.edu.pl", "Tomasz", "Tomaszowski" },
+                    { 3, "anna@annowska.pl", "Anna", "Annowska" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Medicament",
+                columns: new[] { "IdMedicament", "Description", "Name", "Type" },
+                values: new object[,]
+                {
+                    { 1, "Paracetamol", "APAP", "przeciwbólowy" },
+                    { 2, "Ibuprofen", "Ibuprom", "przeciwbólowy" },
+                    { 3, "Witamina C", "Rutinoscorbin", "odpornościowy" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Patient",
+                columns: new[] { "IdPatient", "Birthdate", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2000, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Magdalena", "Lena" },
+                    { 2, new DateTime(2000, 2, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "Katarzyna", "Kata" },
+                    { 3, new DateTime(2000, 3, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), "Aleksandra", "Mandra" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Prescriptions",
+                columns: new[] { "IdPrescription", "Date", "DueDate", "IdDoctor", "IdPatient" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2025, 5, 26, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2025, 6, 9, 0, 0, 0, 0, DateTimeKind.Local), 1, 1 },
+                    { 2, new DateTime(2025, 5, 25, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2025, 6, 2, 0, 0, 0, 0, DateTimeKind.Local), 2, 1 },
+                    { 3, new DateTime(2025, 5, 24, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2025, 6, 2, 0, 0, 0, 0, DateTimeKind.Local), 2, 2 },
+                    { 4, new DateTime(2025, 5, 23, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2025, 6, 2, 0, 0, 0, 0, DateTimeKind.Local), 3, 3 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Prescription_Medicament",
+                columns: new[] { "IdMedicament", "IdPrescription", "Details", "Dose" },
+                values: new object[,]
+                {
+                    { 1, 1, "codziennie wieczorem", 5 },
+                    { 2, 1, "raz na tydzień", null },
+                    { 2, 2, "co 2 dni", 1 },
+                    { 3, 3, "codziennie rano", 2 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Prescription_Medicament",
+                keyColumns: new[] { "IdMedicament", "IdPrescription" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "Prescription_Medicament",
+                keyColumns: new[] { "IdMedicament", "IdPrescription" },
+                keyValues: new object[] { 2, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "Prescription_Medicament",
+                keyColumns: new[] { "IdMedicament", "IdPrescription" },
+                keyValues: new object[] { 2, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "Prescription_Medicament",
+                keyColumns: new[] { "IdMedicament", "IdPrescription" },
+                keyValues: new object[] { 3, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "Prescriptions",
+                keyColumn: "IdPrescription",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Doctor",
+                keyColumn: "IdDoctor",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Medicament",
+                keyColumn: "IdMedicament",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Medicament",
+                keyColumn: "IdMedicament",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Medicament",
+                keyColumn: "IdMedicament",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Patient",
+                keyColumn: "IdPatient",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Prescriptions",
+                keyColumn: "IdPrescription",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Prescriptions",
+                keyColumn: "IdPrescription",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Prescriptions",
+                keyColumn: "IdPrescription",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Doctor",
+                keyColumn: "IdDoctor",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Doctor",
+                keyColumn: "IdDoctor",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Patient",
+                keyColumn: "IdPatient",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Patient",
+                keyColumn: "IdPatient",
+                keyValue: 2);
+        }
+    }
+}
